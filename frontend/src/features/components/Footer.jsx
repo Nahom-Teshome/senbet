0b2a15faeda5b8b3934a3bd13ae3746641c4a0a0
Nahom-Teshome <Nahmelu@gmail.com>
2025-08-20 23:04:40 +0300

    Updated the Events components to render an array of eventDetails prop

--- frontend/src/features/components/Footer.jsx
+++ frontend/src/features/components/Footer.jsx
@@ -1,18 +1,34 @@
-import {Calendar, MenuIcon,MapPin,Clock,BookOpen, CircleCheckBig, Binoculars, Instagram, Facebook, Twitter , Mail, Phone} from 'lucide-react'
+import { Instagram, Facebook, Twitter , Mail, Phone,} from 'lucide-react'
+import { PiWhatsappLogoLight, } from "react-icons/pi";
+import { PiTiktokLogoThin } from "react-icons/pi";
 import { useTranslation } from 'react-i18next'
 export default function Footer(){
     const {t} = useTranslation()
     return(
          <footer id="footer" className="min-h-4/10 md:min-h-3/10 w-full flex bordr  border-red-900 bg-senbet-footer-bg ">
             <div className="h-full w-3/5 md:w-4/5 flex flex-col md:flex md:flex-row gap-2 bordr">
-            <div>
-              <div className="text-heading mt-5 ml-5 m  text-white w-fit h-fit text-start">
+            <div className="">
+              <div className="text-heading mt-5 ml-5 m  bord  r text-white w-full w-fit h-fit text-start">
                 {t('app.name')}
               </div>
              <div className="text-white hidden  borde h-5/10 md:flex md:flex-col items-center justify-center gap-2">
-                    <Facebook strokeWidth={1}/>
-                    <Instagram strokeWidth={1}/>
-                    <Twitter strokeWidth={1}/>
+                    {/* <Facebook strokeWidth={1} className="hover:fill-white"/> */}
+                      <a className="hover:fill-white hover:bg-white hover:text-black text-2xl rounded-xl"
+                        href="https://www.tiktok.com/@kidest0481?_t=ZN-8yeu0UIS7hG&_r=1" target='#'>
+                      <PiTiktokLogoThin />
+                    
+
+                        </a>
+                     <a 
+                        href="https://www.instagram.com/kidest_silasie/profilecard/?igsh=YnF2NWFybDU2bzJs" target='#'>
+                          <Instagram strokeWidth={1} className="hover:fill-white/50"/>
+                        </a>
+                    {/* <Twitter strokeWidth={1} className="hover:fill-white"/> */}
+                   <a className="hover:fill-white hover:bg-white hover:text-black text-2xl rounded-xl"
+                        href="https://chat.whatsapp.com/FW3kNBRh22eF7eFVwW56wl?mode=ac_t" target='#'>
+                     <PiWhatsappLogoLight />
+                  </a>
+                    
                   </div>
             </div>
 
@@ -22,21 +38,32 @@ export default function Footer(){
                     <p className="hidden md:inline
                      text-mediumheading font-light text-white w-fit border-b ">{t('app.contact')}</p>
                   <div className="md:hidden text-white borde h-5/10 flex flex-col pl-10 items-start justify-center gap-2">
-                    <Facebook strokeWidth={1}/>
-                    <Instagram strokeWidth={1}/>
-                    <Twitter strokeWidth={1}/>
+                     {/* <Facebook strokeWidth={1} className="hover:fill-white"/> */}
+                   <a className="hover:fill-white hover:bg-white hover:text-black text-2xl rounded-xl"
+                        href="https://www.tiktok.com/@kidest0481?_t=ZN-8yeu0UIS7hG&_r=1" target='#'>
+                        <PiTiktokLogoThin />
+                        </a>
+                        <a 
+                        href="https://www.instagram.com/kidest_silasie/profilecard/?igsh=YnF2NWFybDU2bzJs" target='#'>
+                          <Instagram strokeWidth={1} className="hover:fill-white/50"/>
+                        </a>
+                        <Twitter strokeWidth={1} className="hover:fill-white"/>
+                     <a className="hover:fill-white hover:bg-white hover:text-black text-2xl rounded-xl"
+                        href="https://chat.whatsapp.com/FW3kNBRh22eF7eFVwW56wl?mode=ac_t" target='#'>
+                     <PiWhatsappLogoLight />
+                  </a>
                   </div>
 
                   <div className="text-white flex gap-2 bor min-w-full wrap-anywhere borde pl-1  md:justify-center md:pl-20 items-center h-5/10  ">
                     <div className="flex flex-col gap-1 md:gap-3   ">
-                      <MapPin strokeWidth={1}/>
-                      <Phone strokeWidth={1} />
-                      <Mail strokeWidth={1}/>
+                      {/* <MapPin strokeWidth={1}/> */}
+                      <Phone strokeWidth={1} className="hover:fill-white" />
+                      <Mail strokeWidth={1} className="hover:fill-white/50"/>
                     </div>
                     <div className="flex text-sm md:text-mediumbody-txt font-light flex-col gap-1 md:gap-3 borde "> 
-                      <p className="text-body-txt font-thin md:text-mediumbody-txt">123 Business</p> 
+                      {/* <p className="text-body-txt font-thin md:text-mediumbody-txt">123 Business</p>  */}
                       <p className="text-body-txt font-thin md:text-mediumbody-txt">+1 (555) 123-4567 </p>
-                      <p className="text-body-txt font-thin md:text-mediumbody-txt">info@yourbusiness.com</p>
+                      <p className="text-body-txt font-thin md:text-mediumbody-txt"> <a href="mailto:luxembourgholytrinity@gmail.com?subject=Inquiry&body=Hello,...">luxembourgholytrinity@gmail.com</a></p>
                     </div>
                     
                   </div>
--- frontend/src/features/components/Nav.jsx
+++ frontend/src/features/components/Nav.jsx
@@ -8,9 +8,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import ChurchIcon from '/churchicon.png'
 import { Switch } from "@/components/ui/switch"
+import { Avatar , AvatarFallback, AvatarImage} from "@/components/ui/avatar"
 import { useTranslation } from 'react-i18next'
-import {Calendar, MenuIcon,MapPin,Clock,BookOpen, CircleCheckBig, Binoculars, Instagram, Facebook, Twitter , Mail, Phone} from 'lucide-react'
+import { MenuIcon} from 'lucide-react'
 export default function Nav(){
     const {i18n,t} = useTranslation()
     const [lang, setLang] = React.useState('am')
@@ -28,7 +30,13 @@ export default function Nav(){
     }
     return(
              <nav className="z-10 h-1/10 w-full top-0 fixed bg-gradient-to-r from-white/69 to-senbet-nav-gradient-gray/69 backdrop-blur-sm drop-shadow-lg drop-shadow-black/50 flex justify-between items-center px-4">
-          <div className='text-title'>{t('app.name')} </div>
+            <div className="flex  items-center gap-2">
+                <Avatar className="size-12">
+                     <AvatarImage src={ChurchIcon} />
+                     <AvatarFallback>Holy Trinity</AvatarFallback>
+                </Avatar>
+              <div className='text-title'>{t('app.name')} </div>
+            </div>
          <div className="text-mediumbody-txt  gap-5 borde items-center text-background hidden md:flex">
               <a href="#events">{t('app.events')}</a>
               <a href="#ourGoals">{t('app.goals')}</a>
